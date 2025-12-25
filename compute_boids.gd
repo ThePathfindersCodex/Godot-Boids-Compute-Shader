bdbfc668b15acd17f677f77ca9d63e47a8c20034
Tokoyuma <84645582+Tokoyuma@users.noreply.github.com>
2025-12-24 20:45:31 -0500

    buffer uniform swap instead of reading from GPU

--- compute_boids.gd
+++ compute_boids.gd
@@ -175,14 +175,44 @@ func run_simulation():
 	for run_mode in [0, 1, 2]:
 		compute_stage(run_mode)
 
-	# Copy GPU out buffers back to in buffers
-	var output_bytes_pos = rdmain.buffer_get_data(buffers[2])  # out_pos_buffer
-	var output_bytes_vel = rdmain.buffer_get_data(buffers[3])  # out_vel_buffer
-	rdmain.buffer_update(buffers[0], 0, output_bytes_pos.size(), output_bytes_pos)  # in_pos_buffer <- out_pos
-	rdmain.buffer_update(buffers[1], 0, output_bytes_vel.size(), output_bytes_vel)  # in_vel_buffer <- out_vel
-
+	# --- Swap results back into input buffers ---
+	swap_buffer_bindings()
+	
 	# UPDATE TEXTURE ON SCREEN
 	texture = textureRD
+
+# FRAME BUFFER SWAP LOGIC
+var swap_flag : int = 0
+func swap_buffer_bindings():
+	if (dt == 0):
+		return
+	
+	swap_flag = 1 - swap_flag   # toggle between 0 and 1
+	var pos_in_index = swap_flag * 2       # 0 or 2
+	var vel_in_index = pos_in_index + 1    # 1 or 3
+	var pos_out_index = 2 - pos_in_index   # if 0to2, if 2to0
+	var vel_out_index = pos_out_index + 1  # 3 or 1
+	
+	# rebuild uniforms for bindings 0 and 1
+	uniforms[0] = RDUniform.new()
+	uniforms[0].uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	uniforms[0].binding = 0
+	uniforms[0].add_id(buffers[pos_in_index])
+	uniforms[1] = RDUniform.new()
+	uniforms[1].uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	uniforms[1].binding = 1
+	uniforms[1].add_id(buffers[vel_in_index])
+	uniforms[2] = RDUniform.new()
+	uniforms[2].uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	uniforms[2].binding = 2
+	uniforms[2].add_id(buffers[pos_out_index])
+	uniforms[3] = RDUniform.new()
+	uniforms[3].uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	uniforms[3].binding = 3
+	uniforms[3].add_id(buffers[vel_out_index])
+	
+	# rebuild uniform set
+	uniform_set = rdmain.uniform_set_create(uniforms, shader, 0)
 
 # HANDLE MOUSE INPUTS
 var dragging := false
